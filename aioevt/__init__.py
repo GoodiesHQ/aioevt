6249c22246eeb812942a14d0e33c02ec45a0f556
GoodiesHQ <GoodiesHQ@users.noreply.github.com>
2018-09-18 16:31:27 -0700

    Update __init__.py
    Added description and version.

--- aioevt/__init__.py
+++ aioevt/__init__.py
@@ -1,11 +1,19 @@
 """
 AIOEVT - Simple Asyncio Event Management
+
+Package exports the event and manager classes.
+
+Managers are used for managing the event list.
+An Event is just a named tuple.
 """
 
 from .event import Event
 from .manager import Manager
 
 name = "aioevt"
+version = (1, 0, 1)
+
+__all__ = ["Event", "Manager", "name", "version"]
 
 __author__ = "Austin Archer"
 __version__ = "0.1.1"
